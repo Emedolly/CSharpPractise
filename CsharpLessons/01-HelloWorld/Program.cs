125c54c012e36e3b12498978ce284caf65808ad5
Emedolly <animelaemeema@gmail.com>
2025-07-23 22:04:52 +0530

    Basic practice programs

--- CsharpLessons/01-HelloWorld/Program.cs
+++ CsharpLessons/01-HelloWorld/Program.cs
@@ -9,23 +9,47 @@ namespace CSharpPractise
         public static void Main()
         {
             Console.WriteLine("Hello World! which is the main entry for these console applications");
-            WhileLoop w = new WhileLoop();
+            //WhileLoop w = new WhileLoop();
             // w.RunForLoop();
             //w.RunWhileLoop();
             //  w.SumOfNumbers();
             // w.FactorialNumbers();
             // w.MultipleOfNumbers();
-         //  Console.WriteLine("Enter the numbers to be reversed");
-          //  int n = int.Parse(Console.ReadLine());
+            //  Console.WriteLine("Enter the numbers to be reversed");
+            //  int n = int.Parse(Console.ReadLine());
             //int result = w.ReversedNumber(n);
             // Console.WriteLine(result);
-           // bool result = w.Palindrome(n);
+            // bool result = w.Palindrome(n);
             //Console.WriteLine(result);
             //w.CountDigits();
-            SwitchExample sw = new SwitchExample();
+            // SwitchExample sw = new SwitchExample();
             //sw.calculator();
-           //sw.DaysoftheWeek();
-           sw.GradeBased();
+            //sw.DaysoftheWeek();
+            // sw.GradeBased();
+
+            // for (int i = 1; i <= 100; i++)
+            // {
+            //  if (i % 7 == 0) To find multiples of 7
+            //  {
+            //   Console.WriteLine(i);
+            //  }
+            // }
+            //Find the maximum number from the array list 
+            // int[] numbers = new int[5];
+            // numbers[0] = 1;
+            // numbers[1] = 2;
+            // numbers[2] = 3;
+            // numbers[3] = 4;
+            // numbers[4] = 5;
+            // var temp = numbers[0];
+            // for (int i = 0; i < 5; i++)
+            // {
+            //     if (numbers[i] >= temp)
+            //     {
+            //         temp = numbers[i];
+            //     }
+            // }
+            // Console.WriteLine(temp);
             Console.ReadLine();
         }
     }
