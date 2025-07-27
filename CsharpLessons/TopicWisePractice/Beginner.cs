cfabfade1719f72a9b1d77a5b34ef2386cd5654e
Emedolly <animelaemeema@gmail.com>
2025-07-27 14:03:58 +0530

    Added summary and added programs for operators in C#

--- CsharpLessons/TopicWisePractice/Beginner.cs
+++ CsharpLessons/TopicWisePractice/Beginner.cs
@@ -1,7 +1,10 @@
+using System.Runtime.ExceptionServices;
+
 namespace CSharpPractise.CsharpLessons.TopicWisePractice;
 
 public class Beginner
 {
+    #region Variables and datatypes
     /// <summary>
     /// Variables and datatypes
     /// </summary>
@@ -12,18 +15,44 @@ public class Beginner
         Console.WriteLine($"Before swapping numbers from {number1} to {number2}");
         number1 = number1 + number2;
         number2 = number1 - number2;
-        number1 = number1-number2;
+        number1 = number1 - number2;
         Console.WriteLine($"After swapping numbers from {number1} to {number2}");
-        
     }
-/// <summary>
-/// EX: 5 as input and output should be 78
-/// </summary>
-/// <param name="radius"></param>
+
+    /// <summary>
+    /// EX: 5 as input and output should be 78
+    /// </summary>
+    /// <param name="radius"></param>
     public void AreaOfCircle(double radius)
     {
         const double PI = 3.14;
         double area = PI * radius * radius;
         Console.WriteLine($"Area is {area}");
     }
+    #endregion
+
+    #region Operators
+
+    /// <summary>
+    /// check if a number is even or odd
+    /// </summary>
+    public void CheckEvenOrOddNumbers()
+    {
+        Console.WriteLine("Enter a  numebr");
+        Int16 number = Int16.Parse(Console.ReadLine());
+        Console.WriteLine((number % 2 == 0) ? "The number is even" : "The number is odd");
+    }
+    /// <summary>
+    /// Find the largest of three numbers using ternary operator
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <param name="c"></param>
+    public void FindLargetNumbers(int a, int b, int c)
+    {
+        // int max = Math.Max(a, Math.Max(b, c));
+        int maxValue = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+        Console.WriteLine($"Max value is {maxValue}");
+    }
+    #endregion
 }
